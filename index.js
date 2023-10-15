0e8f7489a2f47e89aaeb46c24220d99846b1f83f
Ujjwal Raj <ujjwal.2000.raj@gmail.com>
2023-10-15 21:38:43 +0530

    directory structure,setting routes and controllers for homepage

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.use('/',require('./routes'));
+
+
+
+
+
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error in running the server: $(err)`);
+    }
+    console.log(`Server is running on the port: ${port}`);
+});
